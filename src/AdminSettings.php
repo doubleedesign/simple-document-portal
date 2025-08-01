9b2ac9095c235d51aeeeb8dd69d22b72459b59e8
Leesa Ward <leesa@doubleedesign.com.au>
2025-08-01 19:39:56 +1000

    Chore: Clean up redundant/default ACF field settings

--- src/AdminSettings.php
+++ src/AdminSettings.php
@@ -57,75 +57,36 @@ class AdminSettings {
 			'title'                 => 'General',
 			'fields'                => array(
 				array(
-					'key'               => 'field_simple-document-portal__page_title',
-					'label'             => 'Page title',
-					'name'              => 'page_title',
-					'aria-label'        => '',
-					'type'              => 'text',
-					'instructions'      => '',
-					'required'          => 0,
-					'conditional_logic' => 0,
-					'wrapper'           => array(
-						'width' => '',
-						'class' => '',
-						'id'    => '',
-					),
-					'default_value'     => 'Document Portal',
-					'maxlength'         => '',
-					'allow_in_bindings' => 0,
-					'placeholder'       => '',
-					'prepend'           => '',
-					'append'            => '',
+					'key'           => 'field_simple-document-portal__page_title',
+					'label'         => 'Page title',
+					'name'          => 'page_title',
+					'type'          => 'text',
+					'default_value' => 'Document Portal',
+					'maxlength'     => 255
 				),
 				array(
 					'key'               => 'field_document_folder_location',
 					'label'             => 'Server path of document folder',
 					'name'              => 'document_folder_location',
-					'aria-label'        => '',
 					'type'              => 'text',
 					'disabled'          => true,
-					'required'          => 0,
-					'conditional_logic' => 0,
-					'wrapper'           => array(
-						'width' => '',
-						'class' => '',
-						'id'    => '',
-					),
 					'default_value'     => PluginEntrypoint::get_protected_directory(),
-					'maxlength'         => '',
-					'allow_in_bindings' => 0,
-					'placeholder'       => '',
-					'prepend'           => '',
-					'append'            => '',
+					'maxlength'         => 255,
+					'allow_in_bindings' => false
 				),
 				array(
-					'key'               => 'field_6886fadbeabc8',
-					'label'             => 'Portal layout',
-					'name'              => 'portal_layout',
-					'aria-label'        => '',
-					'type'              => 'group',
-					'instructions'      => '',
-					'required'          => 0,
-					'conditional_logic' => 0,
-					'wrapper'           => array(
-						'width' => '',
-						'class' => '',
-						'id'    => '',
-					),
-					'layout'            => 'row',
-					'sub_fields'        => array(
+					'key'        => 'field_6886fadbeabc8',
+					'label'      => 'Portal layout',
+					'name'       => 'portal_layout',
+					'type'       => 'group',
+					'layout'     => 'row',
+					'sub_fields' => array(
 						array(
 							'key'           => 'field_layout_switch_breakpoint',
 							'label'         => 'Layout switch breakpoint',
 							'name'          => 'layout_switch_breakpoint',
 							'type'          => 'number',
 							'instructions'  => 'The document folders will be displayed as an "accordion" unless there is at least this much available space, in which case a "tabs" layout will be used. Use 0 to always show tabs, or a very large number (larger than the maximum available container width you expect) to always show an accordion. Note: This may have no effect if your theme is overriding the template provided by the plugin.',
-							'required'      => 0,
-							'wrapper'       => array(
-								'width' => '',
-								'class' => '',
-								'id'    => '',
-							),
 							'default_value' => 768,
 							'min'           => 0,
 							'max'           => 5000,
@@ -152,7 +113,7 @@ class AdminSettings {
 			'hide_on_screen'        => '',
 			'active'                => true,
 			'description'           => '',
-			'show_in_rest'          => 0,
+			'show_in_rest'          => false,
 		));
 	}
 
@@ -167,62 +128,28 @@ class AdminSettings {
 			'title'                 => 'Messages',
 			'fields'                => array(
 				array(
-					'key'               => 'field_688af60df7c2a',
-					'label'             => 'Permission error',
-					'name'              => 'permission_error_message',
-					'aria-label'        => '',
-					'type'              => 'group',
-					'instructions'      => 'Message shown on the portal page to users who are logged in, but don\'t have permission to read/download documents.',
-					'required'          => 0,
-					'conditional_logic' => 0,
-					'wrapper'           => array(
-						'width' => '',
-						'class' => '',
-						'id'    => '',
-					),
-					'layout'            => 'block',
-					'sub_fields'        => array(
+					'key'          => 'field_688af60df7c2a',
+					'label'        => 'Permission error',
+					'name'         => 'permission_error_message',
+					'type'         => 'group',
+					'instructions' => 'Message shown on the portal page to users who are logged in, but don\'t have permission to read/download documents.',
+					'layout'       => 'block',
+					'sub_fields'   => array(
 						array(
-							'key'               => 'field_688af624f7c2b',
-							'label'             => 'Heading',
-							'name'              => 'heading',
-							'aria-label'        => '',
-							'type'              => 'text',
-							'instructions'      => '',
-							'required'          => 0,
-							'conditional_logic' => 0,
-							'wrapper'           => array(
-								'width' => '',
-								'class' => '',
-								'id'    => '',
-							),
-							'default_value'     => 'No access',
-							'maxlength'         => '',
-							'allow_in_bindings' => 0,
-							'placeholder'       => '',
-							'prepend'           => '',
-							'append'            => '',
+							'key'           => 'field_688af624f7c2b',
+							'label'         => 'Heading',
+							'name'          => 'heading',
+							'type'          => 'text',
+							'default_value' => 'No access',
+							'maxlength'     => 255
 						),
 						array(
-							'key'               => 'field_688af62df7c2c',
-							'label'             => 'Description',
-							'name'              => 'description',
-							'aria-label'        => '',
-							'type'              => 'textarea',
-							'instructions'      => '',
-							'required'          => 0,
-							'conditional_logic' => 0,
-							'wrapper'           => array(
-								'width' => '',
-								'class' => '',
-								'id'    => '',
-							),
-							'default_value'     => 'Sorry, you don\'t have access to the document portal.',
-							'maxlength'         => '',
-							'allow_in_bindings' => 0,
-							'rows'              => 4,
-							'placeholder'       => '',
-							'new_lines'         => '',
+							'key'           => 'field_688af62df7c2c',
+							'label'         => 'Description',
+							'name'          => 'description',
+							'type'          => 'textarea',
+							'default_value' => 'Sorry, you don\'t have access to the document portal.',
+							'rows'          => 4
 						),
 					),
 				),
@@ -244,7 +171,7 @@ class AdminSettings {
 			'hide_on_screen'        => '',
 			'active'                => true,
 			'description'           => '',
-			'show_in_rest'          => 0,
+			'show_in_rest'          => false,
 		));
 	}
 
@@ -272,16 +199,9 @@ class AdminSettings {
 			'fields'                => array_merge([
 				// Add a message field to explain the purpose of this section
 				array(
-					'key'               => 'field_simple-document-portal__document_access_instructions',
-					'type'              => 'message',
-					'message'           => '<p>Permissions are granted to roles upon plugin activation according to a mapping with built-in roles. For example, <code>manage_portal_settings</code> is granted to roles that have the built-in <code>manage_options</code> capability. You can grant or revoke permissions beyond that from here, or make more complex modifications in your own custom user role code (the result of which should be reflected here automatically).</p><p>For simplicity, this form only shows roles that currently have at least one active user on the site.</p>',
-					'required'          => 0,
-					'conditional_logic' => 0,
-					'wrapper'           => array(
-						'width' => '',
-						'class' => '',
-						'id'    => '',
-					),
+					'key'     => 'field_simple-document-portal__document_access_instructions',
+					'type'    => 'message',
+					'message' => '<p>Permissions are granted to roles upon plugin activation according to a mapping with built-in roles. For example, <code>manage_portal_settings</code> is granted to roles that have the built-in <code>manage_options</code> capability. You can grant or revoke permissions beyond that from here, or make more complex modifications in your own custom user role code (the result of which should be reflected here automatically).</p><p>For simplicity, this form only shows roles that currently have at least one active user on the site.</p>',
 				),
 			], $fields),
 			'location'              => array(
@@ -301,7 +221,7 @@ class AdminSettings {
 			'hide_on_screen'        => '',
 			'active'                => true,
 			'description'           => '',
-			'show_in_rest'          => 0,
+			'show_in_rest'          => false,
 		));
 	}
 
@@ -325,25 +245,16 @@ class AdminSettings {
 			'key'               => $field_key,
 			'label'             => $label,
 			'name'              => $capability,
-			'aria-label'        => '',
 			'type'              => 'checkbox',
-			'instructions'      => '',
-			'required'          => 0,
-			'conditional_logic' => 0,
-			'wrapper'           => array(
-				'width' => '',
-				'class' => '',
-				'id'    => '',
-			),
 			'choices'           => $checkboxes,
 			'default_value'     => $preselected,
 			'disabled'          => $non_editable,
 			'return_format'     => 'value',
-			'allow_custom'      => 0,
-			'allow_in_bindings' => 0,
+			'allow_custom'      => false,
+			'allow_in_bindings' => false,
 			'layout'            => 'vertical',
-			'toggle'            => 0,
-			'save_custom'       => 0,
+			'toggle'            => false,
+			'save_custom'       => false,
 		);
 	}
 }
