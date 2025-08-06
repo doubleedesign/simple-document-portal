113245b5cbd33a7b2e1756578d0446fb35f4d290
Leesa Ward <leesa@doubleedesign.com.au>
2025-08-06 10:13:55 +1000

    Only allow for outside-webroot directory for now

--- src/PluginEntrypoint.php
+++ src/PluginEntrypoint.php
@@ -28,11 +28,21 @@ class PluginEntrypoint {
     }
 
     public static function activate(): void {
-        self::create_protected_directory();
-        flush_rewrite_rules();
-        UserRoles::create_roles();
-        UserRoles::reassign_users_roles();
-        UserPermissions::map_permissions_to_existing_roles();
+        try {
+            self::create_protected_directory();
+            flush_rewrite_rules();
+            UserRoles::create_roles();
+            UserRoles::reassign_users_roles();
+            UserPermissions::map_permissions_to_existing_roles();
+        }
+        catch (Exception $e) {
+            self::deactivate();
+            wp_die(
+                esc_html__('Failed to activate Simple Document Portal: ', 'simple-document-portal') . esc_html($e->getMessage()),
+                'Plugin Activation Error',
+                array('back_link' => true)
+            );
+        }
     }
 
     public static function deactivate(): void {
@@ -43,13 +53,26 @@ class PluginEntrypoint {
         delete_option('simple_document_portal_directory');
     }
 
-    public static function uninstall() {
+    public static function uninstall(): void {
         UserRoles::revert_users_roles(true);
         // TODO Clear warning and confirmation that uninstalling will delete the documents
         // TODO Implement deletion
     }
 
+    /**
+     * @throws Exception
+     */
     protected static function create_protected_directory(): void {
+        // Go up a level from the web root and return the full system path
+        $dir = dirname(WP_CONTENT_DIR, 2) . '/documents';
+        if (!file_exists($dir)) {
+            wp_mkdir_p($dir);
+        }
+
+        if (!file_exists($dir) || !is_writable($dir)) {
+            throw new Exception('The documents directory was not able to be created, or is not writable. It should exist one level above the web root (for example, for cPanel hosting this usually means the same level as the `public_html` folder). Please create it manually or update its permissions, and try activating the plugin again.');
+        }
+
         $index_content = <<<PHP
 			<?php
 			// Directory for documents for Simple Document Portal
@@ -59,35 +82,11 @@ class PluginEntrypoint {
 			}
 		PHP;
 
-        try {
-            // Go up a level from the web root and return the full system path
-            $dir = dirname(WP_CONTENT_DIR, 2) . '/documents';
-            if (!file_exists($dir)) {
-                wp_mkdir_p($dir);
-            }
+        file_put_contents($dir . '/index.php', $index_content);
+        self::$directory = $dir;
 
-            file_put_contents($dir . '/index.php', $index_content);
-            self::$directory = $dir;
-
-            // Save location in options table so the admin settings page can access it
-            update_option('simple_document_portal_directory', self::$directory);
-        }
-        catch (Exception $e) {
-            try {
-                $alternateDir = WP_CONTENT_DIR . '/uploads/documents';
-                self::$directory = $alternateDir;
-
-                // Save location in options table so the admin settings page can access it
-                update_option('simple_document_portal_directory', self::$directory);
-
-                // TODO: Warn about protecting this directory
-            }
-            catch (Exception $e) {
-                // TODO: Display an admin notice
-                error_log('Failed to create protected directory for Simple Document Portal: ' . $e->getMessage());
-                self::deactivate();
-            }
-        }
+        // Save location in options table so the admin settings page can access it
+        update_option('simple_document_portal_directory', self::$directory);
     }
 
     public static function get_protected_directory(): string {
